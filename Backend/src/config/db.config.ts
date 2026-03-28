b8ff5d618dc986d805df2ef2c8ba896a823575f4
chowthri2003 <chowthri.thulasidoss@mavens-i.com>
2026-03-28 19:22:45 +0530

    test files removed

--- Backend/src/config/db.config.ts
+++ Backend/src/config/db.config.ts
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const SequelizeConfig = new Sequelize(
-  process.env.DB_NAME || '',
-  process.env.DB_USER || '',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+    port: Number(process.env.DB_PORT),
     dialect: 'postgres',
     logging: false,
     timezone: "+00:00",
